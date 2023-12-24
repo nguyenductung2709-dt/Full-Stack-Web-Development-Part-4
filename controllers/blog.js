5c3a1bd8a635a2172198a0f0af1f08672cffff51
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2023-12-24 17:23:08 +0200

    4.10

---
+++ controllers/blog.js
@@ -0,0 +1,68 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response, next) => {
+  const allBlogs = await Blog.find({})
+  let htmlResponse = '<h1>All Blogs</h1>'
+  htmlResponse += '<ul>'
+  allBlogs.forEach(blog => {
+  htmlResponse += `<li>Title: ${blog.title}, Author: ${blog.author}, Likes: ${blog.likes}</li>`
+  })
+  htmlResponse += '</ul>'
+  response.send(htmlResponse)
+})
+
+blogsRouter.get('/api/blogs', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+blogsRouter.get('/api/blogs/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.delete('/api/blogs/:id', async (request, response, next) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.post('/api/blogs', async (request, response, next) => {
+  const body = request.body
+  if (!body.title || !body.author || !body.url || !body.likes) {
+    return response.status(400).json({ error: 'Required fields are missing' })
+  }
+
+  const newBlog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+  const savedBlog = await newBlog.save()
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.put('/api/blogs/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = blogsRouter
---
+++ test/api_test.test.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const helper = require('./api_test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlog)
+})
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlog.length)
+})
+
+test('all blogs have id property instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+  
+    blogs.forEach(blog => {
+      expect(blog.id).toBeDefined()
+      expect(blog._id).toBeUndefined()
+    })
+  })
+  
+test('a valid blog can be added, and the number of blogs increases ', async () => {
+  const newBlog = {
+    title: "Cloud computing",
+    author: "Tungdt",
+    url: "https://cloudcomputing.com/",
+    likes: 15,
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlog.length + 1)
+
+  const contents = blogsAtEnd.map(n => n.title)
+  expect(contents).toContain(
+    'Cloud computing'
+  )
+})
+
+/*
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  //const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+
+  expect(resultNote.body).toEqual(noteToView)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  expect(notesAtEnd).toHaveLength(
+    helper.initialNotes.length - 1
+  )
+
+  const contents = notesAtEnd.map(r => r.content)
+
+  expect(contents).not.toContain(noteToDelete.content)
+}) */
+
+afterAll(async () => {
+  await mongoose.connection.close()
+}) 
